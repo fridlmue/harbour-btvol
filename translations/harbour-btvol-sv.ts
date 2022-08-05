ba267bb3e932694af5727204fbca4bf6dacdef12
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-08-05 07:01:29 +0200

    Create harbour-btvol-sv.ts
    Add Swedish translation.
    Please let me know when/if you need update.

---
+++ translations/harbour-btvol-sv.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>BT Volume Control</source>
+        <translation>Bluetooth volymkontroll</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Battery: </source>
+        <translation>Batteri: </translation>
+    </message>
+    <message>
+        <source>Device List</source>
+        <translation>Enhetslista</translation>
+    </message>
+    <message>
+        <source>Increase Volume</source>
+        <translation>Höj volymen</translation>
+    </message>
+    <message>
+        <source>Decrease Volume</source>
+        <translation>Sänk volymen</translation>
+    </message>
+    <message>
+        <source>Tap on Device to increase internal Volume</source>
+        <translation>Tryck på enheten för att höja intern volym</translation>
+    </message>
+    <message>
+        <source>Tap on Device to decrease internal Volume</source>
+        <translation>Tryck på enheten för att sänka intern volym</translation>
+    </message>
+</context>
+</TS>
