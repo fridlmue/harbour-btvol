0d6a4b1d4b0dd705addd6bdc2f96ef698ffc3432
fridl <fridl@10hoch-6.de>
2023-05-08 16:46:40 +0200

    l10n update

--- translations/harbour-btvol-sv.ts
+++ translations/harbour-btvol-sv.ts
@@ -2,6 +2,37 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>AboutPage</name>
+    <message>
+        <source>About BtVol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SailfishOS Bluetooth Device Volume Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>By Friedrich Mütschele</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send e-mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Some code is based on snippets from Piggz Amazfish project. https://github.com/piggz/harbour-amazfish</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>CoverPage</name>
     <message>
         <source>BT Volume Control</source>
@@ -27,12 +58,16 @@
         <translation>Sänk volymen</translation>
     </message>
     <message>
-        <source>Tap on Device to increase internal Volume</source>
+        <source>Tap on device to increase internal volume</source>
         <translation>Tryck på enheten för att höja intern volym</translation>
     </message>
     <message>
-        <source>Tap on Device to decrease internal Volume</source>
+        <source>Tap on device to decrease internal volume</source>
         <translation>Tryck på enheten för att sänka intern volym</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
