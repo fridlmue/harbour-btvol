a3f876c3f30983ad3899398995d6292254b502be
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2023-05-08 12:42:05 +0200

    Create harbour-btvol-nl.ts
    Add Dutch translation

---
+++ translations/harbour-btvol-nl.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About BtVol</source>
+        <translation>Over BtVol</translation>
+    </message>
+    <message>
+        <source>SailfishOS Bluetooth Device Volume Control</source>
+        <translation>SailfishOS Bluetooth Apparaatvolumebeheer</translation>
+    </message>
+    <message>
+        <source>By Friedrich Mütschele</source>
+        <translation>Door Friedrich Mütschele</translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation>Zend e-mail</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation>Gelicenseerd onder de GNU GPLv3</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>Broncode op GitHub</translation>
+    </message>
+    <message>
+        <source>Some code is based on snippets from Piggz Amazfish project. https://github.com/piggz/harbour-amazfish</source>
+        <translation>Delen van de code is gebaseerd op Piggz Amazfish project. https://github.com/piggz/harbour-amazfish</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>BT Volume Control</source>
+        <translation>BT Volume Control</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Battery: </source>
+        <translation>Batterij: </translation>
+    </message>
+    <message>
+        <source>Device List</source>
+        <translation>Apparaatlijst</translation>
+    </message>
+    <message>
+        <source>Increase Volume</source>
+        <translation>Volume Verhogen</translation>
+    </message>
+    <message>
+        <source>Decrease Volume</source>
+        <translation>Volume Verlagen</translation>
+    </message>
+    <message>
+        <source>Tap on Device to increase internal Volume</source>
+        <translation>Tik op een apparaat om intern volume te verhogen</translation>
+    </message>
+    <message>
+        <source>Tap on Device to decrease internal Volume</source>
+        <translation>Tik op een apparaat om intern volume te verlagen</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+</context>
+</TS>
